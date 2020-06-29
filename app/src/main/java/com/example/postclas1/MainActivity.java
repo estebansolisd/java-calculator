1ad5e3d32fc207885cd691446518eed8a01d2a6a
estebansolisd <estebansolisd@gmail.com>
2020-06-29 00:44:37 -0600

    Incrementing the reusability

--- app/src/main/java/com/example/postclas1/MainActivity.java
+++ app/src/main/java/com/example/postclas1/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -79,90 +80,65 @@ public class MainActivity extends AppCompatActivity {
                 );
                 break;
         }
+        operator = "";
+        willClear = true;
     }
 
     public void saveValues(){
         if (operator.equals("")){
             firstValue = String.valueOf(txtResult.getText());
-            willClear = true;
         }else{
             secondValue = String.valueOf(txtResult.getText());
-            revealResult();
-            operator = "";
         }
     }
 
-    public void clearChange(){
-        if (willClear) {
-            txtResult.setText("");
-            willClear  = false;
-        }
+    public void setEnabledOperations(boolean flag){
+        findViewById(R.id.btnSub).setEnabled(flag);
+        findViewById(R.id.btnSum).setEnabled(flag);
+        findViewById(R.id.btnDiv).setEnabled(flag);
+        findViewById(R.id.btnMul).setEnabled(flag);
     }
 
+    public void setEnabledEqual(boolean flag){
+        findViewById(R.id.btnEqual).setEnabled(flag);
+    }
 
     public void handleClick(View view){
         switch (view.getId()){
             case R.id.btnOne:
-                clearChange();
-                concatVal("1");
-                break;
             case R.id.btnTwo:
-                clearChange();
-                concatVal("2");
-                break;
             case R.id.btnThree:
-                clearChange();
-                concatVal("3");
-                break;
             case R.id.btnFour:
-                clearChange();
-                concatVal("4");
-                break;
             case R.id.btnFive:
-                clearChange();
-                concatVal("5");
-                break;
             case R.id.btnSix:
-                clearChange();
-                concatVal("6");
-                break;
             case R.id.btnSeven:
-                clearChange();
-                concatVal("7");
-                break;
             case R.id.btnEight:
-                clearChange();
-                concatVal("8");
-                break;
             case R.id.btnNine:
-                clearChange();
-                concatVal("9");
-                break;
             case R.id.btnZero:
-                clearChange();
-                concatVal("0");
+                if (willClear){
+                    txtResult.setText("");
+                    willClear = false;
+                }
+                concatVal(String.valueOf(view.getTag()));
+                setEnabledEqual(!operator.equals(""));
+                setEnabledOperations(operator.equals(""));
+                saveValues();
                 break;
             case R.id.btnClear:
                 txtResult.setText("0");
+                operator = "";
+                willClear = true;
                 break;
             case R.id.btnEqual:
-                saveValues();
+                revealResult();
                 break;
             case R.id.btnSum:
-                saveValues();
-                operator = "sum";
-                break;
-            case R.id.btnSub:
-                saveValues();
-                operator = "sub";
-                break;
-            case R.id.btnMul:
-                saveValues();
-                operator = "mul";
-                break;
             case R.id.btnDiv:
-                saveValues();
-                operator = "div";
+            case R.id.btnMul:
+            case R.id.btnSub:
+                setEnabledOperations(false);
+                operator = String.valueOf(view.getTag());
+                willClear = true;
                 break;
             default:
                 break;
